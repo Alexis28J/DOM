d4842e6f95669ec41ff914ea847f3132ad654eea
Alexis28J <jordy2806@hotmail.com>
2025-11-25 12:52:20 +0100

    Fine lezione (25.11.2025)

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # DOM
 Lezione Martedì 25.11.2025
 Proseguiamo con DOM (Document Object Model)
+
+- Il metodo createTextNode() in JavaScript serve per creare un nuovo nodo di testo nel Document Object Model (DOM) che può essere aggiunto a un elemento HTML esistente. A differenza di createElement(), che crea un nuovo tag HTML, createTextNode() crea solo il testo, senza alcun markup, attributi o elementi figli al suo interno. È utile per inserire del semplice testo in una pagina web, dopo averlo aggiunto a un altro elemento tramite metodi come appendChild().
+
+- Backtick: `  - Alt + 96
+
+
+- In JavaScript, ${} viene usato nei template literal per l'interpolazione di stringhe, permettendo di inserire espressioni, variabili o chiamate di funzione all'interno di una stringa, racchiudendole tra $ e le parentesi graffe {}. Ad esempio, in \Ciao, nome!{nome} verrà sostituito con il valore della variabile nome.
--- script.js
+++ script.js
@@ -48,7 +48,7 @@ console.log(divOne.textContent);  //stampa com'è nel html
 
 
 //innerHTML
-console.log(divOne.innerHTML);     //stampa esattamente il contenuto com'è il codice nell'html
+console.log(divOne.innerHTML);    //stampa esattamente il contenuto com'è il codice nell'html
 
 
 //outerHTML
@@ -60,14 +60,94 @@ console.log(divOne.children);    //collection dei figli
 
 
 
-//DIFFERENZA TRA innerText e textContent  -  VEDI nel CSS la classe .invisible
+//DIFFERENZA TRA innerText e textContent  -  VEDI esempio nel CSS: classe .invisible
+
+//La differenza principale è che innerText restituisce il testo visibile e formattato dall'utente, ignorando il testo nascosto dal CSS, 
+//mentre textContent restituisce il testo raw (grezzo) così come si trova nel markup, incluse le interruzioni di riga e il testo nascosto. 
+//innerText considera gli stili e può essere più lento, specialmente in un ciclo, a causa del costo di calcolo del rendering, mentre textContent è più veloce poiché non deve calcolare lo stile e restituisce il testo non renderizzato.
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//SCRIVERE IL CONTENUTO
+
+//appendChild - METODO più "corretto" in assoluto per aggiunger un elemento creato
+const newP = document.createElement('p');   //si crea un elemento p
+divOne.appendChild(newP);   
+
+//createTextNode - per aggiungere SOLO TESTO con appendChild
+const node = document.createTextNode('Quarto'); //si crea il testo da aggiungere
+newP.appendChild(node);
+
+//innerHTML
+newP.innerHTML = 'Quarto paragrafo';
+newP.innerHTML = '<strong>Quarto paragrafo</strong>';
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//ESEMPIO
+
+const student1 = {name: 'Alexander', surname: 'Macias', age: 26, gender: 'male'};
+const student2 = {name: 'Evelyn', surname: 'Medina', age: undefined, gender: 'female'};
+
+//facciamo una card con appendChild senza usare html
+const emptyDiv = document.getElementById('empty-div');  //chiamo il div vuoto
+const cardDiv = document.createElement('div');   //creo un elemento div che sarà la card
+cardDiv.id = 'first-card';  //aggiuno un identificatore 
+emptyDiv.appendChild(cardDiv);   //metto la card creata al div vuoto
+
+const nameP = document.createElement('p');   //creo un elemento p
+const nameNode = document.createTextNode('Nome: ' + student1.name); //creo un nodo di testo che posso aggiungere a un elemento html
+nameP.appendChild(nameNode); //collego il nodo di testo all'elemento p
+cardDiv.appendChild(nameP); //metto l'elemento p, che a sua volta ha già dentro il nodo di testo nameNode, dentro cardDiv
+
+const surnameP = document.createElement('p');
+const surnameNode = document.createTextNode('Cognome: ' + student1.surname);
+surnameP.appendChild(surnameNode);
+cardDiv.appendChild(surnameP);
+
+//Modo LEGGERMENTE più corto di quelli di prima (nameP e surnameP) - il risultato è lo stesso
+const ageP = document.createElement('p');
+ageP.appendChild(document.createTextNode('Età: ' + student1.age));
+cardDiv.appendChild(ageP)
+
+const genderP = document.createElement('p');
+genderP.appendChild(document.createTextNode('Genere: ' + student1.gender));
+cardDiv.appendChild(genderP);
 
 
+//innerHTML
+
+//CONSIGLIO: INVECE DI SCRIVERE TUTTO IL CODICE, è MEGLIO SFRUTTARE L'AUTOCOMPLETAMENTO DELL'HTML PER, POI, POTER COPIARLO QUA
+const htmlString = `<div class="card">    
+    <p>Nome: ${student2.name}</p>
+    <p>Cognome: ${student2.surname}</p>
+    <p>Età: ${student2.age} </p>
+    <p>Genere: ${student2.gender}</p>
+</div>`;
+
+//Si deve usare per forza con i backstick per mettere il codice!
+//con ${} posso mettere le proprietà degli oggetti (Interpolazione di stringhe)
+
+console.log(htmlString);  //mi stampa student2 nella console
+console.log(emptyDiv.innerHTML);  //qui invece mi stampa student1 
+
+//per aggiungere (renderizzi) student2
+emptyDiv.innerHTML += htmlString;  //è uguale a "emptyDiv.innerHTML = emptyDiv.innerHTML + htmlString"
 
 
+//AGGIUNGERE LE CLASSI
 
+//className
+const card = document.getElementById('first-card');
+//card.className = 'card';
+//card.className = 'card green-background';
+
+//classList - un'alternativa più POTENTE (USEREMO QUESTO INVECE DI CLASSNAME)
+card.classList.add('card');
+card.classList.add('green-background');
+card.classList.remove('card');  //per rimuovere una classe
+card.classList.toggle('card');  //se una classe è gia presente la toglieo viceversa
 
 
 
